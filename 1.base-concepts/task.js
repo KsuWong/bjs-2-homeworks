bff2adbf063309ab1623a440e213065738136857
KsW <kseniawong@ya.ru>
2023-05-02 21:47:21 +0300

    Test

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,12 @@
 "use strict"
 function solveEquation(a, b, c) {
   let arr = [];
-  
+  let discr = b * b - 4 * a * c;
+  let d = discr(a, b, c);
+  if (d < 0) return d;
+
+  let x1 = (- b + Math.sqrt(d)) / (2 * a);
+      x2 = (- b - Math.sqrt(d)) / (2 * a);
   return arr;
 }
 
