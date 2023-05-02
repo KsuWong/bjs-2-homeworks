60540884daa9e2a37fc4a21bc35786a6e67bd7bb
KsW <kseniawong@ya.ru>
2023-05-02 22:28:28 +0300

    Test

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -4,11 +4,11 @@ function solveEquation(a, b, c) {
   let discr = b**2 - 4*a*c;
 
   if (discr < 0) {
-    return arr = [];
+    arr = [];
   } else if (discr > 0) {
     arr = [(-b + Math.sqrt(discr)) / (2 * a), (-b - Math.sqrt(discr)) / (2 * a)];
   } else {
-    arr = [-b/ (2 * a)];
+    arr = [-b / (2 * a)];
   }  
   return arr;
 }
